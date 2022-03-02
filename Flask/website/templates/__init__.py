b8f25766f81e47e5cfebbc5db2676e5bc0b9f32d
infinbomb <99835573+infinbomb@users.noreply.github.com>
2022-03-01 20:47:27 -0500

    as

---
+++ Flask/website/templates/__init__.py
@@ -0,0 +1,14 @@
+# makes this folder a package
+from flask import Flask
+
+def create_app():
+    app = Flask(__name__)
+    app.config['SECRET_KEY'] = 'bob'
+    
+    from .views import views
+    from auth import auth
+    
+    app.register_blueprint(views, url_prefix='/')
+    app.register_blueprint(auth, url_prefix='/')
+    
+    return app
