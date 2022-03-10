d7ba8612ee7b9a4006cedeb3c9ce493f4fd40a73
infinbomb <99835573+infinbomb@users.noreply.github.com>
2022-03-09 22:48:22 -0500

    l

--- IntermediateCode/datetimepractice.py
+++ IntermediateCode/datetimepractice.py
@@ -1,4 +1,42 @@
 import datetime
+import math
+from pickle import PickleError
 
 x = datetime.datetime.now()
 print(x)
+
+print(x.year)
+print(x.strftime("%A"))
+
+# You can use the datetime constructor
+today = datetime.datetime(2022, 3, 9)
+print(today)
+print(today.strftime("%A"))
+print(today.strftime("%c"))
+print(today.strftime("%x"))
+
+
+findmin = min(1, 2, 3)
+findmax = max(1, 2, 3)
+# You can use min and max to find the minadn max
+# in an iterable
+print(findmin)
+print(findmax)
+
+
+findabs = abs(-7.25)
+print(findabs)
+
+power = pow(2, 3)
+print(power)
+
+squareroot = int(math.sqrt(64))
+print(squareroot)
+
+useceiling = math.ceil(1.2)
+usefloor = math.floor(1.7)
+print("Rounding up: " + str(useceiling))
+print("Rounding down: " + str(usefloor))
+
+pie = math.pi
+print(pie)
